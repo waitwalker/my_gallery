a9bd824be34677d35cc4aa7a21e516ae3c7c6e30
liuchuanan <liuchuanan@xianlai-inc.com>
2022-03-28 11:46:50 +0800

    只保留gallery部分，聚焦重点

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_gallery/modules/flu_app/flu_app.dart';
 
+import 'package:my_gallery/main.dart';
+import 'package:my_gallery/modules/flu_app/flu_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
